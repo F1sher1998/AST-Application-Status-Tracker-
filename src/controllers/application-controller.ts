51c87135dc1b84ee15f3fb1be7c125cd05df283d
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-24 15:07:35 +0100

    style/feat: Refactored code
    Removed unnecessary code, stylized and commented for better readability and specifically replaced Allowed filters object with a type instead.

--- src/controllers/application-controller.ts
+++ src/controllers/application-controller.ts
@@ -1,26 +1,23 @@
 import { neon } from "@neondatabase/serverless";
 import type { Request, Response } from "express";
 import { applicationSchema } from "../validator";
-import type { Application } from "../utils/types";
+import { type Application, AllowedFilters } from "../utils/types";
 import { sql } from "../db/Neon/neon-client";
 
 export const createApplication = async(req: Request, res:Response): Promise<Response> => {
-    const accessToken = req.cookies.AccessToken
-    if(!accessToken) res.status(405).send("You are not authorized!")
+
 
     /// Application mandatory data
     const { error, value } = applicationSchema.validate(req.body);
-
     if(error){
         return res.status(400).json({errors: error.details.map(d => d.message)})
     }
 
+    /// Assign validated values
     const body = value as Application
 
     /// Creating an application
     try{
-
-        /// Creating an application
         const [application] = await sql.transaction([sql`
                 INSERT INTO applications (user_id, job_title, status, company, application_date, reached_person)
                 VALUES (${body.userId}, ${body.title}, ${body.status}, ${body.company}, ${body.date}, ${body.person})
@@ -38,8 +35,8 @@ export const createApplication = async(req: Request, res:Response): Promise<Resp
 }
 
 export const findAllApplications = async(req: Request, res:Response): Promise<Response> => {
-    const accessToken = req.cookies.AccessToken
-    if(!accessToken) res.status(405).send("You are not authorized!")
+
+
     /// Finding applications
     try{
 
@@ -60,30 +57,21 @@ export const findAllApplications = async(req: Request, res:Response): Promise<Re
 }
 
 export const FilterApplications = async(req: Request, res:Response): Promise<Response> => {
-    const accessToken = req.cookies.AccessToken
-    if(!accessToken) res.status(405).send("You are not authorized!")
-    /// Allowed Search filters
-    const AllowedFilters = {
-        status: 'status',
-        title: 'job_title',
-        person: 'reached_person',
-        company: 'company',
-        date: 'created_at',
-        user_id: 'user_id',
-        id: 'id'
-    }
+
+    /// Allowed filters
+    const filters = AllowedFilters;
 
     /// Parameter of desired applications
     const {filterType, value} = req.params;
 
+    /// Check if filter type is valid
+    if(!(filterType in filters)){
+        return res.status(400).send("You have provided an invalid filter type")
+    }
+
     /// Chosen filter type for query
-    const chosenFilter = AllowedFilters[filterType]
-
-
-    /// Check if parameters were provided
-    if(!req.params.filterType) return res.status(400).send("You haven't provided a filter");
-    if(!req.params.value) return res.status(400).send("You haven't provided a value");    
-
+    const chosenFilter = filterType as keyof typeof filters;
+    
 
     /// Search for an existing applications
     try{
@@ -108,16 +96,14 @@ export const FilterApplications = async(req: Request, res:Response): Promise<Res
 }
 
 export const updateApplicationStatus = async(req: Request, res:Response): Promise<Response> => {
-    const accessToken = req.cookies.AccessToken
-    if(!accessToken) res.status(405).send("You are not authorized!");
-
+    
     const { appId } = req.params;
     const { status } = req.body;
 
     /// Check if status is provided
     if(!status) return res.status(400).send("You haven't provided a status");
+    
     /// Update application status
-    console.log(appId, status)
     try{
         const updatedApplication = await sql.transaction([sql`
             UPDATE applications
@@ -125,8 +111,11 @@ export const updateApplicationStatus = async(req: Request, res:Response): Promis
             WHERE id = ${appId}
             RETURNING id, job_title, status
         `]);
+
         /// Success message
         return res.status(200).json({message: "Application status has been updated", application: updatedApplication})
+
+    /// Error message
     }catch(error){
         console.log("Error has occured during updating application status")
         return res.status(500).json({message: "Internal server error", error: error});
