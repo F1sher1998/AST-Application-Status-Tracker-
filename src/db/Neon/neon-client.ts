e1d592a7c0a3874cf7a896bd18c17cf248dc3c20
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-24 18:06:39 +0100

    fix: Imported dotenv to neon client

--- src/db/Neon/neon-client.ts
+++ src/db/Neon/neon-client.ts
@@ -1,4 +1,6 @@
 import { neon } from "@neondatabase/serverless";
+import dotenv from 'dotenv';
 
+dotenv.config()
 
 export const sql = neon(process.env.ENVIRONMENT! === 'testing' ? process.env.TEST_DATABASE_URL! : process.env.DEV_DATABASE_URL!)
