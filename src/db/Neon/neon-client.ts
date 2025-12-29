9de99e99ed73169373d6d2eb334e7b939146b87c
Ivan Protsyk <ivanprotsyk1998@gmail.com>
2025-12-29 19:38:13 +0100

    fix: Removed neon client file

--- src/db/Neon/neon-client.ts
+++
@@ -1,6 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import dotenv from 'dotenv';
-
-dotenv.config()
-
-export const sql = neon(process.env.ENVIRONMENT! === 'testing' ? process.env.TEST_DATABASE_URL! : process.env.DEV_DATABASE_URL!)
